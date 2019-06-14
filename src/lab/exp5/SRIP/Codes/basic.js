654aabea81383aac493420d8c43cdbd4686ea0d6
Sindhuja <snadgoud99@gmail.com>
2019-06-14 20:41:51 +0530

    made a different js file and tried out animations

---
+++ src/lab/exp5/SRIP/Codes/basic.js
@@ -0,0 +1,23 @@
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function Benzene() {
+
+    document.getElementById("ttcontent").style.background="#DC143C";
+}
+function Acetaldehyde() {
+
+    document.getElementById("ttcontent").style.background=" #0000FF";
+}
+function Acetate() {
+
+    document.getElementById("ttcontent").style.background="#00FF00";
+}
+
+
+
+
+
+
