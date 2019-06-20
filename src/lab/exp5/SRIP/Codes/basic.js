927d7513e687aa99ff5e47bd30f79157d2ffc1a2
Sindhuja <snadgoud99@gmail.com>
2019-06-20 18:30:28 +0530

    added the basic animations

--- src/lab/exp5/SRIP/Codes/basic.js
+++ src/lab/exp5/SRIP/Codes/basic.js
@@ -17,23 +17,40 @@ function Acetate() {
 function myMove() {
   var elem = document.getElementById("myAnimation");   
   var pos = 0;
-  var id = setInterval(frame, 5);
+  var id = setInterval(frame, .1);
   function frame() 
   {
-    if (pos == 150 ) {
-      clearInterval(id);} 
+    if ( pos==150    ) {
+      clearInterval(id);
+    } 
       else {
-      pos++; 
+        pos++; 
       elem.style.top = pos + 'px'; 
+      
       //elem.style.left = pos + 'px'; 
            }
   }
+}
 
-
-
+function Evaluate()
+{
+  if(document.getElementById("ttcontent").style.background=="#00FF00")
+    window.open("file:///C:/Desktop/GitBash/Git/SRIP/physical-chemistry-iiith/src/lab/exp5/SRIP/Codes/plot_acetaldehyde.html", ' '); 
+  else if (document.getElementById("ttcontent").style.background=="#00FF00")
+    window.open("file:///C:/Desktop/GitBash/Git/SRIP/physical-chemistry-iiith/src/lab/exp5/SRIP/Codes/plot_benzene.html", ' '); 
+  else
+    window.open("file:///C:/Desktop/GitBash/Git/SRIP/physical-chemistry-iiith/src/lab/exp5/SRIP/Codes/plot_ethylacetate.html",' ');
 
 
 }
+
+
+
+
+
+
+
+
 
 
 
