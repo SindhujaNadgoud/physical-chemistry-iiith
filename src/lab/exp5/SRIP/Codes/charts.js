9f0af6ae6dca0413e4c55b45462615eca897ee79
Sindhuja <snadgoud99@gmail.com>
2019-06-30 22:33:12 +0530

    done with graphs,just a lil animation left

--- src/lab/exp5/SRIP/Codes/charts.js
+++ src/lab/exp5/SRIP/Codes/charts.js
@@ -1,5 +1,5 @@
 //window.onload = function () 
-function plot(){
+/*function plot(){
 document.getElementById("chartContainer").style.display = "block";
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
@@ -26,6 +26,105 @@ var chart = new CanvasJS.Chart("chartContainer", {
 			
 		]
 		//labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+	}]
+});
+chart.render();
+
+}
+/*function benzenepress()
+{
+	var chart = new CanvasJS.Chart("benzenecanvas", {
+	animationEnabled: true,
+	exportEnabled: true,
+	
+	axisY:{ 
+		
+		includeZero: true, 
+		interval: 200,
+		gridThickness: .1,
+		
+	},
+	axisX:{ 
+		
+		includeZero: true, 
+		interval: 1,
+		
+		
+	},
+	data: [{
+		type: "stepLine",
+		
+		markerSize: 1,
+		dataPoints: [
+			{ x: 4, y: 240 },
+			{ x: 6, y: 440 },
+			{ x: 9, y: 940 },
+			{ x: 8, y: 1000 },
+			/*{ x: new Date(2016, 1), y: 4 },
+			{ x: new Date(2016, 2), y: 5 },
+			{ x: new Date(2016, 3), y: 5 },
+			{ x: new Date(2016, 4), y: 9 },
+			{ x: new Date(2016, 5), y: 4 },
+			{ x: new Date(2016, 6), y: 7 },
+			{ x: new Date(2016, 7), y: 6 },
+			{ x: new Date(2016, 8), y: 4 },
+			{ x: new Date(2016, 9), y: 4 },
+			{ x: new Date(2016, 10), y: 7 },
+			{ x: new Date(2016, 11), y: 4 },
+			{ x: new Date(2017, 0), y: 9 },
+			{ x: new Date(2017, 1), y: 4 },
+			{ x: new Date(2017, 2), y: 4 },
+			{ x: new Date(2017, 3), y:3 },
+			{ x: new Date(2017, 4), y:3 },
+			{ x: new Date(2017, 5), y:5 }
+		]
+	}]
+});
+chart.render();
+
+}
+
+}*/
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	
+	axisY:{ 
+		
+		includeZero: true, 
+		interval: 200,
+		gridThickness: .1,
+		
+	},
+	axisX:{ 
+		
+		includeZero: false, 
+		interval: 1,
+		 gridThickness: .1,
+		
+		
+	},
+	data: [{
+		type: "stepLine",
+		
+		markerSize: 1,
+		dataPoints: [
+			
+			{ x: 1, y: 0.1 },
+			{ x: 2, y: 0.1 },
+			{ x: 2.7, y: 990 },
+			{ x: 2.7, y: 0.1 },
+			{ x: 4, y: 0.1 },
+			{ x: 5, y: 0.1 },
+			{ x: 6, y: 0.1 },
+			{ x: 7, y: 0.1 },
+			{ x: 8, y: 0.1 },
+			{ x: 9, y: 0.1 },
+			
+			
+		]
 	}]
 });
 chart.render();
