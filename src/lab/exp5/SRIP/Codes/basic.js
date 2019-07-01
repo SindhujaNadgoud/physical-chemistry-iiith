a257650764b6773661176dcbfd78fa68dfb56d98
Sindhuja <snadgoud99@gmail.com>
2019-07-01 19:42:04 +0530

    a lil changes to be done

--- src/lab/exp5/SRIP/Codes/basic.js
+++ src/lab/exp5/SRIP/Codes/basic.js
@@ -15,15 +15,3 @@ function Acetate() {
 
     document.getElementById("ttcontent").style.background="#00FF00";
 }
-function Evaluate()
-{
-  if(document.getElementById("ttcontent").background=="#DC143C")
-    window.open("../../plot_benzene.html", ' '); 
-  else if (document.getElementById("ttcontent").background=="#00FF00")
-    window.open("../../plot_acetaldehyde.html", ' '); 
-  else
-    window.open("../../plot_ethylacetate.html",' ');
-
-
-}
-
--- src/lab/exp5/SRIP/Codes/charts.js
+++ src/lab/exp5/SRIP/Codes/charts.js
@@ -1,92 +1,17 @@
-//window.onload = function () 
-/*function plot(){
-document.getElementById("chartContainer").style.display = "block";
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	theme: "light2",
-	
-	axisY:{
-		includeZero: true
-	},
-	
-    
-	data: [{        
-		type: "line",       
-		dataPoints: [
-			{ y: 0,label:9},
-			{ y: 0,label:8},
-			{ y: 0,label:7 },
-			{ y: 0 ,label:6},
-			{ y: 0 ,label:5},
-			{ y: 190 ,label:4},
-			{ y: 200,label:3 },
-			{ y: 992 ,label:2},
-			{ y: 580 ,label:1},
-			{ y: 0 ,label:0},
-			
-		]
-		//labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-	}]
-});
-chart.render();
-
-}
-/*function benzenepress()
+function plot()
 {
-	var chart = new CanvasJS.Chart("benzenecanvas", {
-	animationEnabled: true,
-	exportEnabled: true,
-	
-	axisY:{ 
-		
-		includeZero: true, 
-		interval: 200,
-		gridThickness: .1,
-		
-	},
-	axisX:{ 
-		
-		includeZero: true, 
-		interval: 1,
-		
-		
-	},
-	data: [{
-		type: "stepLine",
-		
-		markerSize: 1,
-		dataPoints: [
-			{ x: 4, y: 240 },
-			{ x: 6, y: 440 },
-			{ x: 9, y: 940 },
-			{ x: 8, y: 1000 },
-			/*{ x: new Date(2016, 1), y: 4 },
-			{ x: new Date(2016, 2), y: 5 },
-			{ x: new Date(2016, 3), y: 5 },
-			{ x: new Date(2016, 4), y: 9 },
-			{ x: new Date(2016, 5), y: 4 },
-			{ x: new Date(2016, 6), y: 7 },
-			{ x: new Date(2016, 7), y: 6 },
-			{ x: new Date(2016, 8), y: 4 },
-			{ x: new Date(2016, 9), y: 4 },
-			{ x: new Date(2016, 10), y: 7 },
-			{ x: new Date(2016, 11), y: 4 },
-			{ x: new Date(2017, 0), y: 9 },
-			{ x: new Date(2017, 1), y: 4 },
-			{ x: new Date(2017, 2), y: 4 },
-			{ x: new Date(2017, 3), y:3 },
-			{ x: new Date(2017, 4), y:3 },
-			{ x: new Date(2017, 5), y:5 }
-		]
-	}]
-});
-chart.render();
+  if(document.getElementById("ttcontent").style.background=="#DC143C")
+    plotbenzene();
+  else if (document.getElementById("ttcontent").style.background=="#00FF00")
+    plotacetaldehyde();
+  else
+    plotacetate();
+
 
 }
 
-}*/
-window.onload = function () {
-
+function plotbenzene() {
+ document.getElementById("chartContainer").style.display="block";
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	exportEnabled: true,
@@ -112,17 +37,115 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		markerSize: 1,
 		dataPoints: [
 			
-			{ x: 1, y: 0.1 },
-			{ x: 2, y: 0.1 },
+			{ x: 1, y: 0.3 },
+			{ x: 2, y: 0.3 },
 			{ x: 2.7, y: 990 },
-			{ x: 2.7, y: 0.1 },
-			{ x: 4, y: 0.1 },
-			{ x: 5, y: 0.1 },
-			{ x: 6, y: 0.1 },
-			{ x: 7, y: 0.1 },
-			{ x: 8, y: 0.1 },
-			{ x: 9, y: 0.1 },
+			{ x: 2.7, y: 0.3 },
+			{ x: 4, y: 0.3 },
+			{ x: 5, y: 0.3 },
+			{ x: 6, y: 0.3 },
+			{ x: 7, y: 0.3 },
+			{ x: 8, y: 0.3 },
+			{ x: 9, y: 0.3 },
 			
+			
+		]
+	}]
+});
+chart.render();
+
+}
+function plotacetaldehyde() {
+ document.getElementById("chartContainer").style.display="block";
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	
+	axisY:{ 
+		
+		includeZero: true, 
+		interval: 200,
+		gridThickness: .1,
+		
+	},
+	axisX:{ 
+		
+		includeZero: false, 
+		interval: 1,
+		 gridThickness: .1,
+		
+		
+	},
+	data: [{
+		type: "stepLine",
+		
+		markerSize: 1,
+		dataPoints: [
+			
+			{ x: 0.3, y: 210 },
+			{ x: 0.3, y: 0.3 },
+			{ x: 1, y: 0.3 },
+			{ x: 2, y: 0.3 },
+			{ x: 3, y: 0.3 },
+			{ x: 4, y: 0.3 },
+			{ x: 5, y: 0.3 },
+			{ x: 6, y: 0.3 },
+			{ x: 7, y: 0.3 },
+			{ x: 7.8, y: 998 },
+			{ x: 7.8, y: 0.3 },
+			
+			
+			
+		]
+	}]
+});
+chart.render();
+
+}
+function plotacetate() {
+ document.getElementById("chartContainer").style.display="block";
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	
+	axisY:{ 
+		
+		includeZero: true, 
+		interval: 200,
+		gridThickness: .1,
+		
+	},
+	axisX:{ 
+		
+		includeZero: false, 
+		interval: 1,
+		 gridThickness: .1,
+		
+		
+	},
+	data: [{
+		type: "stepLine",
+		
+		markerSize: 1,
+		dataPoints: [
+			
+			{ x: 1, y: 0.3,label: "9"  },
+			{ x: 2, y: 0.3,label: "8"  },
+			{ x: 3, y: 0.3 ,label: "7" },
+			{ x: 4, y: 0.3,label: "6"  },
+			{ x: 5, y: 0.3,label: "5"  },
+			{ x: 5.81, y: 80 },
+			{ x: 5.81, y: 0.3 },
+            { x: 5.87, y: 170 },
+			{ x: 5.87, y: 0.3 },
+			{ x: 5.92, y: 190 },
+			{x:5.92 ,y:0.3},
+			{ x: 6, y: 100,label: "4"  },
+			{ x: 6, y: 0.3,label: "3"  },
+			{ x: 7, y: 0.3 ,label: "2" },
+			{ x: 8, y: 999,label: "1"  },
+			{ x: 8, y: 0.3 },
+			{ x: 9, y: 0.3,label: "0"  },
 			
 		]
 	}]
